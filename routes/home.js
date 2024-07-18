e5c1f3bbc73d31a1625d87319bc9733b6a650fcd
Kcain13 <kevinpcain13@gmail.com>
2024-07-18 19:54:24 -0400

    first commit

---
+++ routes/home.js
@@ -0,0 +1,18 @@
+// Create a routes directory
+mkdir routes
+
+// Create a home.js file in the routes directory
+// routes/home.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Welcome to Fullstack Swing Oil Society</Text>
+    </View>
+  );
+};
+
+export default Home;
+
